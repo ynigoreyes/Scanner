5545edccd3e55354ec4a18f2c00e886b980edc68
Miggy Reyes <ynigoreyes@gmail.com>
2020-03-23 15:10:14 -0500

    adding all test cases

--- Scanner.py
+++ Scanner.py
@@ -1,4 +1,119 @@
+from BadTokenExpection import BadTokenExpection
+
 class Scanner:
     @staticmethod
     def scan(content: str):
-        raise Exception("Not implemented")
+        tokens = []
+        position = 0
+
+        while position < len(content):
+            # cc is short for current character
+            cc = content[position]
+
+            # If the current letter is a string
+            if any(symbol in cc for symbol in ['(', ')', '+', '-']):
+                tokens.append(cc)
+            elif cc.isalpha():
+                finalWord = cc
+                # While the next character is a letter
+                while not Scanner.isAtEndOfString(position, content) and content[position + 1].isalnum():
+                    finalWord += content[position + 1]
+                    position += 1
+                
+                # If the final word is either read or write
+                if any(readOrWrite in finalWord for readOrWrite in ['read', 'write']):
+                    tokens.append(finalWord)
+                else:
+                    tokens.append('id')
+            # If the current character is a digit
+            elif cc.isdigit():
+                # Look for all digits and maybe 1 decimal point
+                seenDecimalPoint = False
+                doneReading = False
+
+                while not doneReading:
+                    if Scanner.isAtEndOfString(position, content):
+                        doneReading = True
+                    elif content[position + 1].isdigit():
+                        position += 1
+                    elif content[position + 1] == '.':
+                        if seenDecimalPoint:
+                            raise BadTokenExpection()
+                        else:
+                            position += 1
+                            seenDecimalPoint = True
+                    elif content[position + 1].isspace():
+                        doneReading = True
+                        position += 1
+                    else:
+                        raise BadTokenExpection()
+                
+                tokens.append('number')
+            # If we see a decimal point
+            elif cc == '.':
+                doneReading = False
+                while not doneReading:
+                    if content[position + 1].isdigit():
+                        position += 1
+                    elif content[position + 1].isspace():
+                        doneReading = True
+                    else:
+                        raise BadTokenExpection()
+
+                tokens.append('number')
+            # Check for proper assignment
+            elif cc == ':':
+                if Scanner.nextCharacterMatches(position, content, '='):
+                    tokens.append('assign')
+                    position += 1
+                else:
+                    raise BadTokenExpection()
+            # Check for division and for comments
+            elif cc == '/':
+                readingComment = True
+
+                while (readingComment):
+                    # Single line comments
+                    if Scanner.nextCharacterMatches(position, content, '/'):
+                        while not Scanner.nextCharacterMatches(position, content, '\n'):
+                            position += 1
+                        readingComment = False
+                    # Multi-line Comments
+                    elif Scanner.nextCharacterMatches(position, content, '*'):
+                        # Look for the */
+                        while True:
+                            if Scanner.nextCharacterMatches(position, content, '*') and Scanner.nextCharacterMatches(position + 1, content, '/'):
+                                position += 2
+                                readingComment = False
+                                break
+                            position += 1
+                    else:
+                        readingComment = False
+                        tokens.append(cc)
+            # Skip all spaces
+            elif cc.isspace():
+                pass
+            # Anything else not recognized by scanner
+            else:
+                raise BadTokenExpection()
+
+            position += 1
+        return tokens
+
+    @staticmethod
+    def nextCharacterMatches(currentPostion: int, content: str, match):
+        if currentPostion + 1 == len(content):
+            return False
+        else:
+            return content[currentPostion + 1] == match
+    
+    @staticmethod
+    def prevCharacterMatches(currentPostion: int, content: str, match):
+        if currentPostion == 0:
+            return False
+        else:
+            return content[currentPostion - 1] == match
+
+    @staticmethod
+    def isAtEndOfString(currentPostion: int, content: str):
+        return currentPostion + 1 == len(content)
--- test.py
+++ test.py
@@ -4,10 +4,102 @@ from BadTokenExpection import BadTokenExpection
 
 class TestScannerFunctionality(unittest.TestCase):
     def test_for_error_on_bad_assignment(self):
+        """
+        read
+        test assignment
+        x := 7
+        y = 7
+        write
+        """
         with open('./test_cases/BadAssignment.txt') as f:
             fileContents = f.read()
             with self.assertRaises(BadTokenExpection):
                 Scanner.scan(fileContents)
+    def test_for_no_error_on_comments(self):
+        """
+        read
+        /**
+        * Here is what a normal doc string looks like
+        */
+        test for comments
+        /**
+        * Here is what a normal doc string looks like
+        */
+        write
+        /**
+        * Here is a sneaky comment at the end
+        */
+        """
+        with open('./test_cases/WithComments.txt') as f:
+            fileContents = f.read()
+            correctTokens = ['read', 'id', 'id', 'id', 'write']
+            tokens = Scanner.scan(fileContents)
+            self.makeSureArraysAreTheSame(tokens, correctTokens)
+    def test_for_no_error_on_duplicates(self):
+        """
+        read write test duplicate another duplicate read write
+        """
+        with open('./test_cases/WithDuplicates.txt') as f:
+            fileContents = f.read()
+            correctTokens = ['read', 'write', 'id', 'id', 'id', 'id', 'read', 'write']
+            tokens = Scanner.scan(fileContents)
+            self.makeSureArraysAreTheSame(tokens, correctTokens)
+            
+    def test_for_no_error_on_symbols_with_comments(self):
+        """
+        // Here is a doc filled with all of the symbols
+        / + - +()
+        """
+        with open('./test_cases/WithSymbolsAndComments.txt') as f:
+            fileContents = f.read()
+            correctTokens = ['/', '+', '-', '+', '(', ')']
+            tokens = Scanner.scan(fileContents)
+            self.makeSureArraysAreTheSame(tokens, correctTokens)
+    def test_for_no_error_on_all_types_of_numbers(self):
+        """
+        0.99
+        0.1
+        1
+        1.0
+        100
+        """
+        with open('./test_cases/AllTypesOfNumbers.txt') as f:
+            fileContents = f.read()
+            correctTokens = ['number', 'number', 'number', 'number', 'number']
+            tokens = Scanner.scan(fileContents)
+            self.makeSureArraysAreTheSame(tokens, correctTokens)
+    def test_for_error_on_bad_numbers(self):
+        """
+        1.1.2
+        """
+        with open('./test_cases/BadNumber.txt') as f:
+            fileContents = f.read()
+            with self.assertRaises(BadTokenExpection):
+                Scanner.scan(fileContents)
+    def test_for_error_on_strings_that_start_with_numbers(self):
+        """
+        4pple
+        """
+        with open('./test_cases/StringStartWithNumbers.txt') as f:
+            fileContents = f.read()
+            with self.assertRaises(BadTokenExpection):
+                Scanner.scan(fileContents)
+    
+    def test_for_no_error_on_strings_that_start_with_letters_but_have_numbers(self):
+        """
+        f00bar m8
+        """
+        with open('./test_cases/StringStartWithLetter.txt') as f:
+            fileContents = f.read()
+            correctTokens = ['id', 'id']
+            tokens = Scanner.scan(fileContents)
+            self.makeSureArraysAreTheSame(tokens, correctTokens)
+    
+    def makeSureArraysAreTheSame(self, array1, array2):
+        self.assertEqual(len(array1), len(array2), array1)
+        for i, j in zip(array1, array2):
+            self.assertEqual(i, j)
+
             
 if __name__ == '__main__':
     unittest.main()
