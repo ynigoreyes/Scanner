a5608055bf273271cf6fe1e3ed61c423657a9e5b
Miggy Reyes <ynigoreyes@gmail.com>
2020-03-23 16:31:34 -0500

    Adding scanning functionality from the command line

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Prerequisites to running:
+* Python 3.7 or higher
 ```
 $ python -V // on windows
 $ python3 -V // on Linux/Mac
@@ -10,6 +11,7 @@ $ python test.py
 ```
 
 ## Running Scanner:
+Place file anywhere and use it's relative path to where you are running the command from
 ```
 $ python main.py <file to read>
 ```
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from Scanner import Scanner
+from pathlib import Path
+
+import sys
 
 def main():
-    scanner = Scanner()
+    fileArg = sys.argv[1]
+    path = Path('.')
+    filePath = path / fileArg
+
+    with open(filePath) as f:
+        content = f.read()
+        try:
+            tokens = Scanner.scan(content)
+            print(tokens)
+        except:
+            print('error')
 
 if __name__ == '__main__':
     main()
